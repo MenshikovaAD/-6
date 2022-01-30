d5da5031e81358cdf5c6e1becc343f336d61c6b4
MenshikovaAD <97692261+MenshikovaAD@users.noreply.github.com>
2022-01-30 14:28:57 +0300

    Add files via upload

---
+++ Задача 6.2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Метод находит самое длиное слово в предложении
+            Console.Write("Введите предложение, разделение слов предусмотрено пробелом, знаки препинания не используются" + Environment.NewLine);
+
+            string startString = Console.ReadLine();
+            string str2 = startString.ToLower();
+            string[] str3 = str2.Split();
+            string str4 = string.Join("", str3);
+            string str5 = new string(str4.Reverse().ToArray());
+            Console.Write(" " + Environment.NewLine);    
+            if (str4 == str5) 
+                {
+                Console.Write("Слово является полиандромом");
+                }
+            else
+            {
+                Console.Write("Слово не является полиандромом");
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
